0f56c48c3ccf0fe9663385978dc0d401da541d86
Djimaraes <guiguimaraes@live.com.pt>
2018-11-05 17:46:05 +0000

    meu perfil

--- app/src/main/java/com/example/android/buyshare/Perfil.java
+++ app/src/main/java/com/example/android/buyshare/Perfil.java
@@ -9,8 +9,5 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-        getSupportActionBar().setTitle("Meu Perfil");
-
-
     }
 }
