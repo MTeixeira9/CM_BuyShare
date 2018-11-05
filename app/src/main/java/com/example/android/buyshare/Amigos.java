8f4dab1ecf6e9e97cc00d0c9e0e13cf2a156e804
Djimaraes <guiguimaraes@live.com.pt>
2018-11-05 18:42:31 +0000

    login

--- app/src/main/java/com/example/android/buyshare/Amigos.java
+++ app/src/main/java/com/example/android/buyshare/Amigos.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -49,8 +50,8 @@ public class Amigos extends AppCompatActivity {
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1){
-            if (resultCode == RESULT_OK){
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
                 String nome = data.getStringExtra("nomeA");
                 String tlmv = data.getStringExtra("nTlm");
                 String novoAmigo = nome + " " + tlmv;
