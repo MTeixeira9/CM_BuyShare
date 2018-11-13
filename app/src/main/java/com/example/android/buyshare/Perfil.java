b91786fe5488cddfe8e2876503cc6d1fc7c3b437
Djimaraes <guiguimaraes@live.com.pt>
2018-11-13 19:40:37 +0000

    Profil and edit_profil

--- app/src/main/java/com/example/android/buyshare/Perfil.java
+++ app/src/main/java/com/example/android/buyshare/Perfil.java
@@ -1,9 +1,18 @@
 package com.example.android.buyshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Perfil extends AppCompatActivity {
+
+
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +20,36 @@ public class Perfil extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
 
         getSupportActionBar().setTitle("Meu Perfil");
-        
+
+        Button alteraDados = (Button) findViewById(R.id.alterarDados);
+
+        alteraDados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Perfil.this, EditPerfil.class);
+                startActivityForResult(i, 1);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                String nome = data.getStringExtra("nome");
+                String password = data.getStringExtra("pwd");
+                String nTlm = data.getStringExtra("nTlm");
+                String email = data.getStringExtra("email");
+
+                TextView nomeTV = (TextView) findViewById(R.id.nome_perfil);
+                TextView pwdTV = (TextView) findViewById(R.id.pwd_perfil);
+                TextView nTlm_TV = (TextView) findViewById(R.id.nTlm_perfil);
+                TextView email_TV = (TextView) findViewById(R.id.email_perfil);
+
+                nomeTV.setText(nome);
+                pwdTV.setText(password);
+                nTlm_TV.setText(nTlm);
+                email_TV.setText(email);
+            }
+        }
     }
 }
