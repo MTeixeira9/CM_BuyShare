c5de2fbc87221e101cb8a1bee6b3e3a0969a091e
MTeixeira9 <mario_ze16@hotmail.com>
2018-12-04 15:45:25 +0000

    registo

--- app/src/main/java/com/example/android/buyshare/Database/User.java
+++ app/src/main/java/com/example/android/buyshare/Database/User.java
@@ -1,5 +1,9 @@
 package com.example.android.buyshare.Database;
 
+import com.example.android.buyshare.LoginActivity;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class User {
 
     public String nome;
@@ -16,6 +20,18 @@ public class User {
         this.password = password;
         this.numeroTlm = numeroTlm;
         this.email = email;
+    }
+
+    public static void writeNewUser(String userId, String name, String pass, String nTel, String email) {
+        User user = new User(name, pass, nTel, email);
+
+        LoginActivity.mDatabase.child("users").child(userId).setValue(user);
+    }
+
+    public static void readUser(String name) {
+        /*User user = new User(name, pass, nTel, email);
+
+        LoginActivity.mDatabase.child("users").child(userId).setValue(user);*/
     }
 
 }
--- app/src/main/java/com/example/android/buyshare/LoginActivity.java
+++ app/src/main/java/com/example/android/buyshare/LoginActivity.java
@@ -13,7 +13,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase;
+    public static DatabaseReference mDatabase;
 
 
     @Override
