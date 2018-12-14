321844c29828fdb5eb6af791e8a072a48573c458
Djimaraes <guiguimaraes@live.com.pt>
2018-12-14 18:57:48 +0000

    a

--- app/src/main/java/com/example/android/buyshare/FragAddMembrosAmigos.java
+++ app/src/main/java/com/example/android/buyshare/FragAddMembrosAmigos.java
@@ -1,17 +1,31 @@
 package com.example.android.buyshare;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import com.example.android.buyshare.Database.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 
 public class FragAddMembrosAmigos extends Fragment {
 
     private String userTlm;
-    private Da
+    private ArrayAdapter<String> mAdapter;
+
 
     public FragAddMembrosAmigos() {
         // Required empty public constructor
@@ -21,15 +35,8 @@ public class FragAddMembrosAmigos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-         View v =  inflater.inflate(R.layout.frag_add_membros_amigos, container, false);
+        return inflater.inflate(R.layout.frag_add_membros_amigos, container, false);
 
-        userTlm = getActivity().getIntent().getStringExtra("userTlm");
-
-
-
-
-
-         return v;
     }
 
 
